557cacf55bcfe0a0dae0df22d04f4fd7b38f3fa3
Norman Koch <norman.koch@tu-dresden.de>
2023-05-20 15:09:24 +0200

    docker works

--- test.php
+++ test.php
@@ -4,41 +4,22 @@
 ?>
 	<h3><?php print $GLOBALS["databaseName"].".".$GLOBALS["collectionName"]; ?> on <?php print $GLOBALS["mongodbHost"].":".$GLOBALS["mongodbPort"]; ?></h3>
 <?php
+	// Specify the database and collection
+	$databaseName = $GLOBALS["databaseName"];
+	$collectionName = $GLOBALS["collectionName"];
 
-	$filter = [
-		'name' => $GLOBALS["collectionName"],
-		'type' => 'collection',
-	];
-
-	// Selecting collection using MongoDB\Driver\Command
-	$command = new MongoDB\Driver\Command([
-		'listCollections' => 1,
-		'filter' => $filter
-	]);
-
-	$cursor = $GLOBALS["mongoClient"]->executeCommand($GLOBALS["databaseName"], $command);
-
-	check_cursor_object($cursor);
-
-	if ($cursor->valid()) {
-		$collection = $cursor->current()->name;
-		echo "Collection found: " . $collection;
-	} else {
-		echo "Collection not found. Here's what I tried:<br>\n";
-		print("<pre>=================<br>\n");
-		print_r($command);
-		print("\n=================<br></pre>");
-		print("Database name: " . $GLOBALS["databaseName"] . "<br>\n");
-		return;
-	}
-
-	// Access the selected collection
+	// Build the query
 	$query = new MongoDB\Driver\Query([]);
 
-	$cursor = $GLOBALS["mongoClient"]->executeQuery($GLOBALS["namespace"], $query);
+	// Execute the query
+	$cursor = $GLOBALS["mongoClient"]->executeQuery("$databaseName.$collectionName", $query);
+	#check_cursor_object($cursor);
 
+	// Process the cursor results
 	foreach ($cursor as $document) {
-		// Process each document in the collection
-		var_dump($document);
+		// Process each document
+		echo "<pre>";
+		print_r($document);
+		echo "</pre>";
 	}
 ?>

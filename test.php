cec5bafe109d7593eca7fbca55f1dcefd8fd9d16
Norman Koch <norman.koch@tu-dresden.de>
2023-05-20 15:10:00 +0200

    docker works

--- test.php
+++ test.php
@@ -4,15 +4,11 @@
 ?>
 	<h3><?php print $GLOBALS["databaseName"].".".$GLOBALS["collectionName"]; ?> on <?php print $GLOBALS["mongodbHost"].":".$GLOBALS["mongodbPort"]; ?></h3>
 <?php
-	// Specify the database and collection
-	$databaseName = $GLOBALS["databaseName"];
-	$collectionName = $GLOBALS["collectionName"];
-
 	// Build the query
 	$query = new MongoDB\Driver\Query([]);
 
 	// Execute the query
-	$cursor = $GLOBALS["mongoClient"]->executeQuery("$databaseName.$collectionName", $query);
+	$cursor = $GLOBALS["mongoClient"]->executeQuery($GLOBALS["namespace"], $query);
 	#check_cursor_object($cursor);
 
 	// Process the cursor results

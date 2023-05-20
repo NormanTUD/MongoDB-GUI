54c8bab420ad37cd5266ef8e7bafc15370f67dc8
Norman Koch <norman.koch@tu-dresden.de>
2023-05-20 16:12:14 +0200

    docker works

--- test.php
+++ test.php
@@ -7,55 +7,58 @@ include("functions.php");
 <?php
 // Function to recursively extract fields from nested documents
 function extractFields($document, $parentField = '', &$fields = []) {
-    foreach ($document as $key => $value) {
-        $field = $parentField ? $parentField . '.' . $key : $key;
-        $fields[] = $field;
+	foreach ($document as $key => $value) {
+		$field = $parentField ? $parentField . '.' . $key : $key;
+		$fields[] = $field;
 
-        echo "<pre>Extracted field: $field</pre>\n";
-
-        if (is_array($value) && !empty($value)) {
-            extractFields($value, $field, $fields); // Recurse for nested associative arrays
-        } elseif (is_array($value)) {
-            foreach ($value as $subValue) {
-                if (is_array($subValue) && !empty($subValue)) {
-                    extractFields($subValue, $field, $fields); // Recurse for nested associative arrays within arrays
-                }
-            }
-        }
-    }
+		if (is_array($value) && !empty($value)) {
+			if (isAssocArray($value)) {
+				extractFields($value, $field, $fields); // Recurse for nested associative arrays
+			} else {
+				foreach ($value as $index => $subValue) {
+					$subField = $field . '.' . $index;
+					$fields[] = $subField;
+					extractFields($subValue, $subField, $fields); // Recurse for nested associative arrays within arrays
+				}
+			}
+		}
+	}
 }
+
+
+
 
 
 
 // Function to determine the data type of a field
 function determineFieldType($document, $field) {
-    $fieldParts = explode('.', $field);
-    $value = $document;
+	$fieldParts = explode('.', $field);
+	$value = $document;
 
-    foreach ($fieldParts as $part) {
-        if (isset($value[$part])) {
-            $value = $value[$part];
-        } else {
-            return 'string';
-        }
-    }
+	foreach ($fieldParts as $part) {
+		if (isset($value[$part])) {
+			$value = $value[$part];
+		} else {
+			return 'string';
+		}
+	}
 
-    if (is_array($value)) {
-        return 'array'; // Field contains an array
-    }
+	if (is_array($value)) {
+		return 'array'; // Field contains an array
+	}
 
-    if (is_numeric($value)) {
-        return 'double';
-    } elseif (is_bool($value)) {
-        return 'boolean';
-    } else {
-        return 'string';
-    }
+	if (is_numeric($value)) {
+		return 'double';
+	} elseif (is_bool($value)) {
+		return 'boolean';
+	} else {
+		return 'string';
+	}
 }
 
 // Function to determine if an array is associative or indexed
 function isAssocArray($array) {
-    return array_keys($array) !== range(0, count($array) - 1);
+	return array_keys($array) !== range(0, count($array) - 1);
 }
 
 // Retrieve the list of fields from the database
@@ -63,19 +66,25 @@ $query = new MongoDB\Driver\Query([], ['projection' => ['_id' => 0]]);
 $cursor = $GLOBALS["mongoClient"]->executeQuery($GLOBALS["namespace"], $query);
 
 $fields = [];
+$documents = [];
 foreach ($cursor as $document) {
-    extractFields((array)$document, '', $fields);
+	#extractFields((array)$document, '', $fields);
+	$documentArray = json_decode(json_encode($document), true);
+	$documents[] = $documentArray;
 }
 
+dier($documents);
+
+/*
 // Generate options and filters
 $options = [];
 $filters = [];
 
 foreach ($fields as $field) {
     $options[] = [
-        'id' => $field,
-        'label' => $field,
-        'type' => determineFieldType((array)$document, $field)
+	'id' => $field,
+	'label' => $field,
+	'type' => determineFieldType((array)$document, $field)
     ];
 
     $fieldParts = explode('.', $field);
@@ -84,21 +93,21 @@ foreach ($fields as $field) {
     $operators = ['equal', 'not_equal'];
     $fieldType = determineFieldType((array)$document, $field);
     if ($fieldType === 'double' || $fieldType === 'boolean') {
-        $operators[] = 'less';
-        $operators[] = 'less_or_equal';
-        $operators[] = 'greater';
-        $operators[] = 'greater_or_equal';
+	$operators[] = 'less';
+	$operators[] = 'less_or_equal';
+	$operators[] = 'greater';
+	$operators[] = 'greater_or_equal';
     } elseif ($fieldType === 'string') {
-        $operators[] = 'contains'; // Add 'contains' for string fields
+	$operators[] = 'contains'; // Add 'contains' for string fields
     }
 
     $filters[] = [
-        'id' => $field,
-        'label' => $field,
-        'type' => $fieldType,
-        'operators' => $operators,
-        'input' => $fieldType === 'string' ? 'text' : 'number',
-        'field' => $field // Use the full field path as the filter field
+	'id' => $field,
+	'label' => $field,
+	'type' => $fieldType,
+	'operators' => $operators,
+	'input' => $fieldType === 'string' ? 'text' : 'number',
+	'field' => $field // Use the full field path as the filter field
     ];
 }
 
@@ -109,5 +118,5 @@ print_r($options);
 echo "\nFilters:\n";
 print_r($filters);
 echo "</pre>";
+ */
 ?>
-

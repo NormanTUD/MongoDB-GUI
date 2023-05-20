773d96681aacff03fad51ca2fda81655405ecc0e
Norman Koch <norman.koch@tu-dresden.de>
2023-05-20 16:19:03 +0200

    docker works

--- test.php
+++ test.php
@@ -5,7 +5,11 @@ include("functions.php");
 <h3><?php echo $GLOBALS["databaseName"] . "." . $GLOBALS["collectionName"]; ?> on <?php echo $GLOBALS["mongodbHost"] . ":" . $GLOBALS["mongodbPort"]; ?></h3>
 
 <?php
-	$optionsAndFilters = generateQueryBuilderOptions($mongoClient, $namespace, $databaseName, $collectionName, $mongodbHost, $mongodbPort);
+	$optionsAndFilters = generateQueryBuilderOptions($GLOBALS["mongoClient"], $GLOBALS["namespace"], $GLOBALS["databaseName"], $GLOBALS["collectionName"], $GLOBALS["mongodbHost"], $GLOBALS["mongodbPort"]);
 
-	echo json_encode($optionsAndFilters);
+	$options = $optionsAndFilters["options"];
+	$filters = $optionsAndFilters["filters"];
+
+	echo json_encode($options);
+	echo json_encode($filters);
 ?>

6ae6bdecc2b486fbf3655f7f7c6b5cda8f35bdb1
Norman Koch <norman.koch@tu-dresden.de>
2023-05-21 00:46:34 +0200

    fix

--- insert.php
+++ insert.php
@@ -4,42 +4,42 @@ include("functions.php");
 // Function to insert a document
 function insertDocument($document)
 {
-    $bulkWrite = new MongoDB\Driver\BulkWrite();
-    $bulkWrite->insert($document);
+	$bulkWrite = new MongoDB\Driver\BulkWrite();
+	$bulkWrite->insert($document);
 
-    try {
-        $GLOBALS["mongoClient"]->executeBulkWrite($GLOBALS["namespace"], $bulkWrite);
-        return json_encode(['success' => 'Entry created successfully.']);
-    } catch (Exception $e) {
-        return json_encode(['error' => 'Error creating entry: ' . $e->getMessage()]);
-    }
+	try {
+		$GLOBALS["mongoClient"]->executeBulkWrite($GLOBALS["namespace"], $bulkWrite);
+		return json_encode(['success' => 'Entry created successfully.']);
+	} catch (Exception $e) {
+		return json_encode(['error' => 'Error creating entry: ' . $e->getMessage()]);
+	}
 }
 
 // Process form submission
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['data'])) {
-    $data = $_POST['data'];
+	$data = $_POST['data'];
 
-    // Detect data format
-    $jsonData = json_decode($data, true);
-    if ($jsonData !== null && json_last_error() === JSON_ERROR_NONE) {
-        echo insertDocument($jsonData);
-    } else {
-        $lines = explode(PHP_EOL, $data);
-        $headers = str_getcsv(array_shift($lines));
+	// Detect data format
+	$jsonData = json_decode($data, true);
+	if ($jsonData !== null && json_last_error() === JSON_ERROR_NONE) {
+		echo insertDocument($jsonData);
+	} else {
+		$lines = explode(PHP_EOL, $data);
+		$headers = str_getcsv(array_shift($lines));
 
-        $documents = [];
-        foreach ($lines as $line) {
-            $row = str_getcsv($line);
-            $document = array_combine($headers, $row);
-            $documents[] = $document;
-        }
+		$documents = [];
+		foreach ($lines as $line) {
+			$row = str_getcsv($line);
+			$document = array_combine($headers, $row);
+			$documents[] = $document;
+		}
 
-        if (!empty($documents)) {
-            echo insertDocument($documents);
-        } else {
-            echo json_encode(['error' => 'Invalid data format']);
-        }
-    }
+		if (!empty($documents)) {
+			echo insertDocument($documents);
+		} else {
+			echo json_encode(['error' => 'Invalid data format']);
+		}
+	}
 }
 ?>
 <!DOCTYPE html>
@@ -49,10 +49,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['data'])) {
 </head>
 <body>
     <form method="post">
-        <textarea name="data" rows="10" cols="50"></textarea>
-        <br>
-        <input type="submit" value="Submit">
+	<textarea name="data" rows="10" cols="50"></textarea>
+	<br>
+	<input type="submit" value="Submit">
     </form>
 </body>
 </html>
-

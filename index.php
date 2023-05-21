9ba6d2319578b3a674311f243bb6607b5c43c8f2
Norman Koch <norman.koch@tu-dresden.de>
2023-05-21 14:12:54 +0200

    fix

--- index.php
+++ index.php
@@ -179,39 +179,8 @@ $jsCode = generateVisualizationCode($entries, $analyze_fields);
 ?>
 			//} jsCode
 
-			var options = removeDuplicates(<?php echo json_encode($options); ?>);
-			options = [options[0]];
 
-			var filters = removeDuplicates(<?php print json_encode($filters); ?>);
-
-			if(filters.length) {
-				$('#builder-basic').queryBuilder({
-					plugins: ["bt-tooltip-errors"],
-					filters: filters,
-					rules: options
-				});
-				$("#search_stuff").show();
-			} else {
-				$("#search_stuff").hide();
-				console.log("No DB entries found");
-			}
-
-			$('#btn-reset').on('click', function () {
-				$('#builder-basic').queryBuilder('reset');
-			});
-
-			$('#btn-set').on('click', function () {
-				$('#builder-basic').queryBuilder('setRules', JSON.parse(options));
-			});
-
-			$('#btn-get').on('click', function () {
-				var result = $('#builder-basic').queryBuilder('getRules');
-
-				if (!$.isEmptyObject(result)) {
-					alert(JSON.stringify(result, null, 2));
-				}
-			});
-
+			<?php include("initialize_query_builder.php"); ?>
 			<?php include("sample_map.php"); ?>
 			<?php include("sample_analyze.php"); ?>
 		</script>

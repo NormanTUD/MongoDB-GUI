51fea18f6830c49eb0347d65d0155302a99afb24
Norman Koch <norman.koch@tu-dresden.de>
2023-05-24 09:34:54 +0200

    kinda trains

--- questionnaire.php
+++ questionnaire.php
@@ -277,137 +277,99 @@ if (isset($_SERVER["REQUEST_METHOD"]) && $_SERVER['REQUEST_METHOD'] === 'POST') 
             margin-right: 5px;
         }
     </style>
+<?php
+	include("headers.php");
+?>
 	<script>
 const language = {
-  en: {
-    name_question: 'What is your name?',
-    age_question: 'How old are you?',
-    gender_question: 'Whats your gender?',
-    male_option: 'male',
-    female_option: 'female',
-    other_option: 'other',
-    reading_option: 'reading',
-    sports_option: 'sports',
-    music_option: 'music',
-    traveling_option: 'traveling',
-    address_question: 'address?',
-    country_residence_question: 'country of residence?',
-    title: 'Questionnaire',
-    submit: 'Submit',
-    required_question: 'Required question not answered: ',
-    invalid_response: 'Invalid response for question: ',
-    h1: 'Questionnaire',
-    not_in_browser: 'Not in a browser-like environment. Are you calling this from the CLI?',
-    select_option: 'Select an option',
-    address_label: 'Address',
-    street_label: 'Street',
-    city_label: 'City',
-    state_label: 'State',
-    country_label: 'Country',
-    hobbies_question: 'What are your hobbies?',
-    personal_information: 'Personal information',
-    location_question: 'Location'
-  },
-  de: {
-    name_question: 'Wie ist dein Name?',
-    age_question: 'Wie alt bist du?',
-    gender_question: 'Was ist dein Geschlecht?',
-    male_option: 'männlich',
-    female_option: 'weiblich',
-    other_option: 'anderes',
-    reading_option: 'Lesen',
-    sports_option: 'Sport',
-    music_option: 'Musik',
-    traveling_option: 'Reisen',
-    address_question: 'Adresse?',
-    country_residence_question: 'Land des Wohnsitzes?',
-    title: 'Fragebogen',
-    submit: 'Absenden',
-    required_question: 'Erforderliche Frage nicht beantwortet: ',
-    invalid_response: 'Ungültige Antwort für Frage: ',
-    h1: 'Fragebogen',
-    not_in_browser: 'Nicht in einer browserähnlichen Umgebung. Rufen Sie dies aus der Befehlszeile auf?',
-    select_option: 'Option auswählen',
-    address_label: 'Adresse',
-    street_label: 'Straße',
-    city_label: 'Stadt',
-    state_label: 'Bundesland',
-    country_label: 'Land',
-    hobbies_question: 'Was sind deine Hobbys?',
-    personal_information: 'Persönliche Informationen',
-    location_question: 'Ort'
-  },
-  ja: {
-    name_question: 'お名前は何ですか？',
-    age_question: '年齢はいくつですか？',
-    gender_question: '性別は何ですか？',
-    male_option: '男性',
-    female_option: '女性',
-    other_option: 'その他',
-    reading_option: '読書',
-    sports_option: 'スポーツ',
-    music_option: '音楽',
-    traveling_option: '旅行',
-    address_question: '住所は？',
-    country_residence_question: '居住国はどこですか？',
-    title: 'アンケート',
-    submit: '送信',
-    required_question: '必須の質問が回答されていません: ',
-    invalid_response: '無効な回答です: ',
-    h1: 'アンケート',
-    not_in_browser: 'ブラウザのような環境ではありません。CLI から呼び出していますか？',
-    select_option: 'オプションを選択',
-    address_label: '住所',
-    street_label: '番地',
-    city_label: '市区町村',
-    state_label: '都道府県',
-    country_label: '国',
-    hobbies_question: '趣味は何ですか？',
-    personal_information: '個人情報',
-    location_question: '位置'
-  }
-  // Add more language translations here
+	en: {
+		name_question: 'What is your name?',
+		age_question: 'How old are you?',
+		gender_question: 'Whats your gender?',
+		male_option: 'male',
+		female_option: 'female',
+		other_option: 'other',
+		reading_option: 'reading',
+		sports_option: 'sports',
+		music_option: 'music',
+		traveling_option: 'traveling',
+		address_question: 'address?',
+		country_residence_question: 'country of residence?',
+		title: 'Questionnaire',
+		submit: 'Submit',
+		required_question: 'Required question not answered: ',
+		invalid_response: 'Invalid response for question: ',
+		h1: 'Questionnaire',
+		not_in_browser: 'Not in a browser-like environment. Are you calling this from the CLI?',
+		select_option: 'Select an option',
+		address_label: 'Address',
+		street_label: 'Street',
+		city_label: 'City',
+		state_label: 'State',
+		country_label: 'Country',
+		hobbies_question: 'What are your hobbies?',
+		personal_information: 'Personal information',
+		location_question: 'Location'
+	},
+	de: {
+		name_question: 'Wie ist dein Name?',
+		age_question: 'Wie alt bist du?',
+		gender_question: 'Was ist dein Geschlecht?',
+		male_option: 'männlich',
+		female_option: 'weiblich',
+		other_option: 'anderes',
+		reading_option: 'Lesen',
+		sports_option: 'Sport',
+		music_option: 'Musik',
+		traveling_option: 'Reisen',
+		address_question: 'Adresse?',
+		country_residence_question: 'Land des Wohnsitzes?',
+		title: 'Fragebogen',
+		submit: 'Absenden',
+		required_question: 'Erforderliche Frage nicht beantwortet: ',
+		invalid_response: 'Ungültige Antwort für Frage: ',
+		h1: 'Fragebogen',
+		not_in_browser: 'Nicht in einer browserähnlichen Umgebung. Rufen Sie dies aus der Befehlszeile auf?',
+		select_option: 'Option auswählen',
+		address_label: 'Adresse',
+		street_label: 'Straße',
+		city_label: 'Stadt',
+		state_label: 'Bundesland',
+		country_label: 'Land',
+		hobbies_question: 'Was sind deine Hobbys?',
+		personal_information: 'Persönliche Informationen',
+		location_question: 'Ort'
+	},
+	ja: {
+		name_question: 'お名前は何ですか？',
+		age_question: '年齢はいくつですか？',
+		gender_question: '性別は何ですか？',
+		male_option: '男性',
+		female_option: '女性',
+		other_option: 'その他',
+		reading_option: '読書',
+		sports_option: 'スポーツ',
+		music_option: '音楽',
+		traveling_option: '旅行',
+		address_question: '住所は？',
+		country_residence_question: '居住国はどこですか？',
+		title: 'アンケート',
+		submit: '送信',
+		required_question: '必須の質問が回答されていません: ',
+		invalid_response: '無効な回答です: ',
+		h1: 'アンケート',
+		not_in_browser: 'ブラウザのような環境ではありません。CLI から呼び出していますか？',
+		select_option: 'オプションを選択',
+		address_label: '住所',
+		street_label: '番地',
+		city_label: '市区町村',
+		state_label: '都道府県',
+		country_label: '国',
+		hobbies_question: '趣味は何ですか？',
+		personal_information: '個人情報',
+		location_question: '位置'
+	}
 };
-
-var lang = 'de';
-
-// Get the language from the query string parameter 'lang'
-const urlParams = new URLSearchParams(window.location.search);
-const lang = urlParams.get('lang') || 'en'; // Default language is English
-
-// Function to update the translation of elements
-function updateTranslations() {
-  const elements = document.getElementsByClassName('TRANSLATEME');
-  for (const element of elements) {
-    const translationKey = element.classList[0];
-    const translation = language[lang][translationKey];
-    element.textContent = translation;
-  }
-}
-
-// Update translations on initial page load
-updateTranslations();
-
-// Update translations when language selector links are clicked
-const languageSelectors = document.getElementsByClassName('language-selector');
-for (const selector of languageSelectors) {
-  selector.addEventListener('click', function (event) {
-    event.preventDefault();
-    const selectedLang = this.getAttribute('href').substr(6);
-    urlParams.set('lang', selectedLang);
-    window.location.search = urlParams.toString();
-  });
-}
-
-// Update translations when language is changed via URL parameter
-window.addEventListener('popstate', function () {
-  const newLang = urlParams.get('lang') || 'en';
-  if (newLang !== lang) {
-    lang = newLang;
-    updateTranslations();
-  }
-});
-
 </script>
     <title><?php echo getTranslation('title'); ?></title>
 </head>
@@ -468,6 +430,45 @@ window.addEventListener('popstate', function () {
     <?php endforeach; ?>
     <button type="submit"><?php echo getTranslation('submit', true); ?></button>
 </form>
+	<script>
+// Get the language from the query string parameter 'lang'
+const urlParams = new URLSearchParams(window.location.search);
+let lang = urlParams.get('lang') || 'en'; // Default language is English
+
+// Function to update the translation of elements
+function updateTranslations() {
+	const elements = document.querySelectorAll('[class^="TRANSLATEME_"]');
+	elements.forEach((element) => {
+		const translationKey = element.classList[0].substring(12);
+		log(translationKey);
+		const translation = language[lang][translationKey];
+		element.textContent = translation;
+	});
+}
+
+// Update translations on initial page load
+updateTranslations();
+
+// Update translations when language selector links are clicked
+const languageSelectors = document.getElementsByClassName('language-selector');
+Array.from(languageSelectors).forEach((selector) => {
+selector.addEventListener('click', function (event) {
+	event.preventDefault();
+	const selectedLang = this.getAttribute('href').substr(6);
+	urlParams.set('lang', selectedLang);
+	window.location.search = urlParams.toString();
+});
+});
+
+// Update translations when language is changed via URL parameter
+window.addEventListener('popstate', function () {
+	const newLang = urlParams.get('lang') || 'en';
+	if (newLang !== lang) {
+		lang = newLang;
+		updateTranslations();
+	}
+});
+</script>
 </body>
 
 </html>

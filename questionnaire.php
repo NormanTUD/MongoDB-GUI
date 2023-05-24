d76a21d84986e5a4aa3753a1f27cd21b7ed1d674
Norman Koch <norman.koch@tu-dresden.de>
2023-05-24 08:23:41 +0200

    kinda trains

--- questionnaire.php
+++ questionnaire.php
@@ -3,34 +3,94 @@ define('INCLUDED_FROM_INDEX', true);
 include_once("functions.php");
 // Language file containing translations
 $language = [
-    'en' => [
-	    'name_question' => 'What is your name?',
-	    'age_question' => 'How old are you?',
-	    'gender_question' => 'Whats your gender?',
-	    'male_option' => 'male',
-	    'female_option' => 'female',
-	    'other_option' => 'other',
-	    'reading_option' => 'reading',
-	    'sports_option' => 'sports',
-	    'music_option' => 'music',
-	    'traveling_option' => 'traveling',
-	    'address_question' => 'address?',
-	    'country_residence_question' => 'country of residence?',
-        'title' => 'Questionnaire',
-        'submit' => 'Submit',
-        'required_question' => 'Required question not answered: ',
-        'invalid_response' => 'Invalid response for question: ',
-        'h1' => 'Questionnaire',
-        'not_in_browser' => 'Not in a browser-like environment. Are you calling this from the CLI?',
-        'select_option' => 'Select an option',
-        'address_label' => 'Address',
-        'street_label' => 'Street',
-        'city_label' => 'City',
-	'state_label' => 'State',
-	'country_label' => 'Country',
-	'hobbies_question' => 'What are your hobbies?'
-    ],
-    // Add more language translations here
+	'en' => [
+		'name_question' => 'What is your name?',
+		'age_question' => 'How old are you?',
+		'gender_question' => 'Whats your gender?',
+		'male_option' => 'male',
+		'female_option' => 'female',
+		'other_option' => 'other',
+		'reading_option' => 'reading',
+		'sports_option' => 'sports',
+		'music_option' => 'music',
+		'traveling_option' => 'traveling',
+		'address_question' => 'address?',
+		'country_residence_question' => 'country of residence?',
+		'title' => 'Questionnaire',
+		'submit' => 'Submit',
+		'required_question' => 'Required question not answered: ',
+		'invalid_response' => 'Invalid response for question: ',
+		'h1' => 'Questionnaire',
+		'not_in_browser' => 'Not in a browser-like environment. Are you calling this from the CLI?',
+		'select_option' => 'Select an option',
+		'address_label' => 'Address',
+		'street_label' => 'Street',
+		'city_label' => 'City',
+		'state_label' => 'State',
+		'country_label' => 'Country',
+		'hobbies_question' => 'What are your hobbies?',
+		'personal_information' => 'Personal information',
+		'location_question' => 'Location'
+	],
+	'de' => [
+		'name_question' => 'Wie ist dein Name?',
+		'age_question' => 'Wie alt bist du?',
+		'gender_question' => 'Was ist dein Geschlecht?',
+		'male_option' => 'männlich',
+		'female_option' => 'weiblich',
+		'other_option' => 'anderes',
+		'reading_option' => 'Lesen',
+		'sports_option' => 'Sport',
+		'music_option' => 'Musik',
+		'traveling_option' => 'Reisen',
+		'address_question' => 'Adresse?',
+		'country_residence_question' => 'Land des Wohnsitzes?',
+		'title' => 'Fragebogen',
+		'submit' => 'Absenden',
+		'required_question' => 'Erforderliche Frage nicht beantwortet: ',
+		'invalid_response' => 'Ungültige Antwort für Frage: ',
+		'h1' => 'Fragebogen',
+		'not_in_browser' => 'Nicht in einer browserähnlichen Umgebung. Rufen Sie dies aus der Befehlszeile auf?',
+		'select_option' => 'Option auswählen',
+		'address_label' => 'Adresse',
+		'street_label' => 'Straße',
+		'city_label' => 'Stadt',
+		'state_label' => 'Bundesland',
+		'country_label' => 'Land',
+		'hobbies_question' => 'Was sind deine Hobbys?',
+		'personal_information' => 'Persönliche Informationen',
+		'location_question' => 'Ort'
+	],
+	'ja' => [
+		'name_question' => 'お名前は何ですか？',
+		'age_question' => '年齢はいくつですか？',
+		'gender_question' => '性別は何ですか？',
+		'male_option' => '男性',
+		'female_option' => '女性',
+		'other_option' => 'その他',
+		'reading_option' => '読書',
+		'sports_option' => 'スポーツ',
+		'music_option' => '音楽',
+		'traveling_option' => '旅行',
+		'address_question' => '住所は？',
+		'country_residence_question' => '居住国はどこですか？',
+		'title' => 'アンケート',
+		'submit' => '送信',
+		'required_question' => '必須の質問が回答されていません: ',
+		'invalid_response' => '無効な回答です: ',
+		'h1' => 'アンケート',
+		'not_in_browser' => 'ブラウザのような環境ではありません。CLI から呼び出していますか？',
+		'select_option' => 'オプションを選択',
+		'address_label' => '住所',
+		'street_label' => '番地',
+		'city_label' => '市区町村',
+		'state_label' => '都道府県',
+		'country_label' => '国',
+		'hobbies_question' => '趣味は何ですか？',
+		'personal_information' => '個人情報',
+		'location_question' => '位置'
+	],
+	// Add more language translations here
 ];
 
 // Function to retrieve language translations
@@ -55,7 +115,7 @@ function getTranslation($key) {
 // Sample list of questions and input types
 $questions = [
 	[
-		'group' => 'Personal Information',
+		'group' => getTranslation('personal_information'),
 		'questions' => [
 			[
 				'question' => getTranslation('name_question'),
@@ -68,76 +128,76 @@ $questions = [
 				'input_type' => 'number',
 				'required' => true,
 				'name' => 'age'
-            ],
-            [
-                'question' => getTranslation('gender_question'),
-                'input_type' => 'radio',
-                'name' => 'gender',
-                'options' => [
-                    getTranslation('male_option'),
-                    getTranslation('female_option'),
-                    getTranslation('other_option')
-                ]
-            ]
-        ]
-    ],
-    [
-        'group' => 'Hobbies',
-        'questions' => [
-            [
-                'question' => getTranslation('hobbies_question'),
-                'input_type' => 'checkbox',
-                'name' => 'hobbies',
-                'options' => [
-                    getTranslation('reading_option'),
-                    getTranslation('sports_option'),
-                    getTranslation('music_option'),
-                    getTranslation('traveling_option')
-                ]
-            ]
-        ]
-    ],
-    [
-        'group' => 'Location',
-        'questions' => [
-            [
-                'question' => getTranslation('address_question'),
-                'input_type' => 'address',
-                'required' => true,
-		'name' => 'location',
-                'fields' => [
-                    [
-                        'label' => getTranslation('street_label'),
-                        'name' => 'street'
-                    ],
-                    [
-                        'label' => getTranslation('city_label'),
-                        'name' => 'city'
-                    ],
-                    [
-                        'label' => getTranslation('state_label'),
-                        'name' => 'state'
-                    ],
-                    [
-                        'label' => getTranslation('country_label'),
-                        'name' => 'country'
-                    ]
-                ]
-            ],
-            [
-                'question' => getTranslation('country_residence_question'),
-                'input_type' => 'select',
-                'name' => 'country',
-                'options' => [
-                    'USA',
-                    'Canada',
-                    'UK',
-                    'Australia',
-                    getTranslation('other_option')
-                ]
-            ]
-        ]
-    ]
+			],
+			[
+				'question' => getTranslation('gender_question'),
+				'input_type' => 'radio',
+				'name' => 'gender',
+				'options' => [
+					getTranslation('male_option'),
+					getTranslation('female_option'),
+					getTranslation('other_option')
+				]
+			]
+		]
+	],
+	[
+		'group' => 'Hobbies',
+		'questions' => [
+			[
+				'question' => getTranslation('hobbies_question'),
+				'input_type' => 'checkbox',
+				'name' => 'hobbies',
+				'options' => [
+					getTranslation('reading_option'),
+					getTranslation('sports_option'),
+					getTranslation('music_option'),
+					getTranslation('traveling_option')
+				]
+			]
+		]
+	],
+	[
+		'group' => getTranslation('location_question'),
+		'questions' => [
+			[
+				'question' => getTranslation('address_question'),
+				'input_type' => 'address',
+				'required' => true,
+				'name' => 'location',
+				'fields' => [
+					[
+						'label' => getTranslation('street_label'),
+						'name' => 'street'
+					],
+					[
+						'label' => getTranslation('city_label'),
+						'name' => 'city'
+					],
+					[
+						'label' => getTranslation('state_label'),
+						'name' => 'state'
+					],
+					[
+						'label' => getTranslation('country_label'),
+						'name' => 'country'
+					]
+				]
+			],
+			[
+				'question' => getTranslation('country_residence_question'),
+				'input_type' => 'select',
+				'name' => 'country',
+				'options' => [
+					'USA',
+					'Canada',
+					'UK',
+					'Australia',
+					getTranslation('other_option')
+				]
+			]
+		]
+	]
 ];
 
 // Function to validate and process the form submission
@@ -151,7 +211,7 @@ function processFormSubmission($questions)
             $name = $question['name'];
             $value = $_POST[$name] ?? '';
 
-            if ($question['required'] && empty($value)) {
+            if (isset($question['required']) && $question['required'] && empty($value)) {
                 $errors[] = getTranslation('required_question') . $question['question'];
             }
 

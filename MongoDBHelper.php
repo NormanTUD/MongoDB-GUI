d94b062582f85662e3927014b93126d8cb3cc9fb
Norman Koch <norman.koch@tu-dresden.de>
2023-05-24 21:10:38 +0200

    fix

--- MongoDBHelper.php
+++ MongoDBHelper.php
@@ -16,12 +16,10 @@ class MongoDBHelper {
 	}
 
 	private function debug ($msg) {
-		if($this->enableDebug) {
 			print("=================");
 			print_r($msg);
 			debug_print_backtrace();
 			print("=================");
-		}
 	}
 
 	private function newBulkWrite () {

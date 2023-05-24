297addb18a54f787a2e8690ca56db10c5501a76f
Norman Koch <norman.koch@tu-dresden.de>
2023-05-24 18:16:35 +0200

    fix

--- questionnaire.php
+++ questionnaire.php
@@ -17,7 +17,6 @@ include_once("functions.php");
 	include("headers.php");
 ?>
 	<script>
-		const language = <?php print json_encode($language); ?>;
 		const questions = <?php print json_encode($questions); ?>;
 
 		function getFormData($form){
@@ -97,48 +96,6 @@ include_once("functions.php");
     <button onclick='submitForm(event);' type="submit"><?php echo getTranslation('submit'); ?></button>
 </form>
 	<script>
-// Get the language from the query string parameter 'lang'
-const urlParams = new URLSearchParams(window.location.search);
-let lang = urlParams.get('lang') || 'en'; // Default language is English
-
-function setLang (l) {
-	lang = l;
-	updateTranslations();
-}
-
-// Function to update the translation of elements
-function updateTranslations() {
-	const elements = document.querySelectorAll('[class^="TRANSLATEME_"]');
-	elements.forEach((element) => {
-		const translationKey = element.classList[0].substring(12);
-		const translation = language[lang][translationKey];
-		if(translation) {
-			element.textContent = translation;
-		} else {
-			alert("Could not translate " + translationKey + " to " + lang);
-		}
-	});
-}
-
-// Update translations on initial page load
-updateTranslations();
-
-// Update translations when language selector links are clicked
-var languageSelectors = $(".language-selector").find("span")
-Array.from(languageSelectors).forEach((selector) => {
-	selector.addEventListener('click', function (event) {
-		event.preventDefault();
-	});
-});
-
-// Update translations when language is changed via URL parameter
-window.addEventListener('popstate', function () {
-	const newLang = urlParams.get('lang') || 'en';
-	if (newLang !== lang) {
-		lang = newLang;
-		updateTranslations();
-	}
-});
 </script>
 </body>
 

39867e2ca538f2665f3a8a95feadc84d3885fce6
Norman Koch <norman.koch@tu-dresden.de>
2023-05-24 10:04:23 +0200

    kinda trains

--- questionnaire.php
+++ questionnaire.php
@@ -100,13 +100,13 @@ $language = [
 		'USA' => 'USA',
 		'UK' => 'UK',
 		'Canada' => 'Canada',
-		'Australia' => 'Australien'
+		'Australia' => 'Australia'
 	],
 	// Add more language translations here
 ];
 
 // Function to retrieve language translations
-function getTranslation($key, $span = false) {
+function getTranslation($key, $span = false, $option = false) {
     global $language;
     $lang = 'en'; // Default language is English
 
@@ -117,7 +117,12 @@ function getTranslation($key, $span = false) {
     if (isset($language[$lang])) {
         if (isset($language[$lang][$key])) {
             if ($span) {
-                return '<span class="TRANSLATEME_'.$key.'"></span>';
+		    if($option) {
+			    return '<option class="TRANSLATEME_'.$key.'">'.$key.'</option>';
+		    } else {
+			    return '<span value="'.$key.'" class="TRANSLATEME_'.$key.'">'.$key.'</span>';
+		    }
+
             } else {
                 return $language[$lang][$key];
             }
@@ -281,95 +286,7 @@ if (isset($_SERVER["REQUEST_METHOD"]) && $_SERVER['REQUEST_METHOD'] === 'POST') 
 	include("headers.php");
 ?>
 	<script>
-const language = {
-	en: {
-		name_question: 'What is your name?',
-		age_question: 'How old are you?',
-		gender_question: 'Whats your gender?',
-		male_option: 'male',
-		female_option: 'female',
-		other_option: 'other',
-		reading_option: 'reading',
-		sports_option: 'sports',
-		music_option: 'music',
-		traveling_option: 'traveling',
-		address_question: 'address?',
-		country_residence_question: 'country of residence?',
-		title: 'Questionnaire',
-		submit: 'Submit',
-		required_question: 'Required question not answered: ',
-		invalid_response: 'Invalid response for question: ',
-		h1: 'Questionnaire',
-		not_in_browser: 'Not in a browser-like environment. Are you calling this from the CLI?',
-		select_option: 'Select an option',
-		address_label: 'Address',
-		street_label: 'Street',
-		city_label: 'City',
-		state_label: 'State',
-		country_label: 'Country',
-		hobbies_question: 'What are your hobbies?',
-		personal_information: 'Personal information',
-		location_question: 'Location'
-	},
-	de: {
-		name_question: 'Wie ist dein Name?',
-		age_question: 'Wie alt bist du?',
-		gender_question: 'Was ist dein Geschlecht?',
-		male_option: 'männlich',
-		female_option: 'weiblich',
-		other_option: 'anderes',
-		reading_option: 'Lesen',
-		sports_option: 'Sport',
-		music_option: 'Musik',
-		traveling_option: 'Reisen',
-		address_question: 'Adresse?',
-		country_residence_question: 'Land des Wohnsitzes?',
-		title: 'Fragebogen',
-		submit: 'Absenden',
-		required_question: 'Erforderliche Frage nicht beantwortet: ',
-		invalid_response: 'Ungültige Antwort für Frage: ',
-		h1: 'Fragebogen',
-		not_in_browser: 'Nicht in einer browserähnlichen Umgebung. Rufen Sie dies aus der Befehlszeile auf?',
-		select_option: 'Option auswählen',
-		address_label: 'Adresse',
-		street_label: 'Straße',
-		city_label: 'Stadt',
-		state_label: 'Bundesland',
-		country_label: 'Land',
-		hobbies_question: 'Was sind deine Hobbys?',
-		personal_information: 'Persönliche Informationen',
-		location_question: 'Ort'
-	},
-	ja: {
-		name_question: 'お名前は何ですか？',
-		age_question: '年齢はいくつですか？',
-		gender_question: '性別は何ですか？',
-		male_option: '男性',
-		female_option: '女性',
-		other_option: 'その他',
-		reading_option: '読書',
-		sports_option: 'スポーツ',
-		music_option: '音楽',
-		traveling_option: '旅行',
-		address_question: '住所は？',
-		country_residence_question: '居住国はどこですか？',
-		title: 'アンケート',
-		submit: '送信',
-		required_question: '必須の質問が回答されていません: ',
-		invalid_response: '無効な回答です: ',
-		h1: 'アンケート',
-		not_in_browser: 'ブラウザのような環境ではありません。CLI から呼び出していますか？',
-		select_option: 'オプションを選択',
-		address_label: '住所',
-		street_label: '番地',
-		city_label: '市区町村',
-		state_label: '都道府県',
-		country_label: '国',
-		hobbies_question: '趣味は何ですか？',
-		personal_information: '個人情報',
-		location_question: '位置'
-	}
-};
+const language = <?php print json_encode($language); ?>;
 </script>
     <title><?php echo getTranslation('title'); ?></title>
 </head>
@@ -422,12 +339,14 @@ const language = {
                 <select name="<?php echo $question['name']; ?>">
                     <option value=""><?php echo getTranslation('select_option', true); ?></option>
                     <?php foreach ($question['options'] as $option): ?>
-                        <option value="<?php echo $option; ?>"><?php echo getTranslation($option, true); ?></option>
+			<?php echo getTranslation($option, true, true); ?>
                     <?php endforeach; ?>
                 </select>
             <?php endif; ?>
         <?php endforeach; ?>
     <?php endforeach; ?>
+	<br>
+	<br>
     <button type="submit"><?php echo getTranslation('submit', true); ?></button>
 </form>
 	<script>
@@ -446,7 +365,13 @@ function updateTranslations() {
 	elements.forEach((element) => {
 		const translationKey = element.classList[0].substring(12);
 		const translation = language[lang][translationKey];
-		element.textContent = translation;
+		if(translation) {
+			element.textContent = translation;
+			log(element);
+			log(translation);
+		} else {
+			alert("Could not translate " + translationKey + " to " + lang);
+		}
 	});
 }
 
